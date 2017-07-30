6fe0d31fa668f370bac3ea6ebff166d4ad1fbd92
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-07-29 23:35:39 -0700

    simple animations

--- js/index.js
+++ js/index.js
@@ -6,7 +6,27 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         sectionsColor:['','#f7f9ff','#b0adb5','#f7f9ff','#b0adb5'],
         anchors: ['home', 'portfolio', 'about','skills', 'contact'],
-        menu: '#navigation'
+        menu: '#navigation',
+        onLeave: function(index, nextIndex, direction){
+            $('.animation').removeClass('animated zoomInRight');
+            $('.animation').removeClass('animated zoomInRight');
+            $('#quote').removeClass('animated pulse');
+        },
+        afterLoad:function(anchorLink, index){
+		var loadedSection = $(this);
+    		if(anchorLink == 'contact'){
+    			$('.animation').addClass('animated zoomInRight');
+    		}
+            if(anchorLink == 'about'){
+    			$('.entrance').addClass('animated fadeIn');
+                $('#quote').addClass('animated pulse');
+    		}
+        },
+        afterRender: function(){
+            $('.animation').addClass('animated zoomInRight');
+            $('.animation').addClass('animated zoomInRight');
+            $('#quote').addClass('animated pulse');
+        }
     });
     /*============================================
     Skills
